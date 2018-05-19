b367eac1f284d44dde4111fcc4ba499eb913b56a
Babubhai <shubhsharma10@gmail.com>
2018-05-19 18:09:43 -0400

    added course model

---
+++ src/main/java/com/example/springwebdevssharma/repositories/CourseRepository.java
@@ -0,0 +1,8 @@
+package com.example.springwebdevssharma.repositories;
+
+import com.example.springwebdevssharma.models.Course;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, Integer> {
+
+}
---
+++ src/main/java/com/example/springwebdevssharma/repositories/ModuleRepository.java
@@ -0,0 +1,8 @@
+package com.example.springwebdevssharma.repositories;
+
+import com.example.springwebdevssharma.models.Module;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ModuleRepository extends CrudRepository<Module, Integer> {
+
+}
