77edde78e5c0869966649f35634d8a9230906d50
Babubhai <shubhsharma10@gmail.com>
2018-05-23 07:02:40 -0400

    Added update course

--- src/main/java/com/example/springwebdevssharma/services/CourseService.java
+++ src/main/java/com/example/springwebdevssharma/services/CourseService.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +36,18 @@ public class CourseService {
     Optional<Course> data = courseRepository.findById(courseId);
     if(data.isPresent()) {
       return data.get();
+    }
+    return null;
+  }
+
+  @PutMapping("/api/course/{courseId}")
+  public Course updateCourse(@PathVariable("courseId") int courseId, @RequestBody Course newCourse) {
+    Optional<Course> data = courseRepository.findById(courseId);
+    if(data.isPresent()) {
+      Course course = data.get();
+      course.setModified(newCourse.getModified());
+      courseRepository.save(course);
+      return course;
     }
     return null;
   }
