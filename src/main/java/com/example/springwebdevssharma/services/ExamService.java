a5518a4a8957a4c0075f8a3a388e6636a7d937f5
Babubhai <shubhsharma10@gmail.com>
2018-06-03 18:02:52 -0400

    Added create/update/delete exam

--- src/main/java/com/example/springwebdevssharma/services/ExamService.java
+++ src/main/java/com/example/springwebdevssharma/services/ExamService.java
@@ -12,8 +12,12 @@ import com.example.springwebdevssharma.repositories.TrueFalseQuestionRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -34,6 +38,60 @@ public class ExamService {
 
   @Autowired
   MultipleChoiceQuestionRepository multipleChoiceQuestionRepository;
+
+  @PostMapping("/api/topic/{tid}/exam")
+  public Exam createExam(@PathVariable("tid") int topicId, @RequestBody Exam newExam) {
+    Optional<Topic> data = topicRepository.findById(topicId);
+    if(data.isPresent()) {
+      Topic topic = data.get();
+      newExam.setTopic(topic);
+      return examRepository.save(newExam);
+    }
+    return null;
+  }
+
+  @PutMapping("/api/exam/{eid}")
+  public Exam updateExam(@PathVariable("eid") int eid, @RequestBody Exam newExam) {
+    Optional<Exam> data = examRepository.findById(eid);
+    if(data.isPresent()) {
+      Exam exam = data.get();
+      exam.setTitle(newExam.getTitle());
+      return examRepository.save(exam);
+    }
+    return null;
+  }
+
+  @DeleteMapping("/api/exam/{eid}")
+  public void deleteExam(@PathVariable("eid") int widgetId) {
+    examRepository.deleteById(widgetId);
+  }
+
+  @GetMapping("/api/exam/{examId}/question")
+  public List<Question> findAllQuestionsForExam(@PathVariable("examId") int examId) {
+    Optional<Exam> optionalExam = examRepository.findById(examId);
+    if(optionalExam.isPresent()) {
+      Exam exam = optionalExam.get();
+      List<Question> questions = exam.getQuestions();
+      return questions;
+    }
+    return null;
+  }
+
+  @PostMapping("/api/exam/{eid}/truefalse")
+  public TrueFalseQuestion createTrueFalseQuestion(@PathVariable("eid") int examId, @RequestBody TrueFalseQuestion newQuestion) {
+    Optional<Exam> data = examRepository.findById(examId);
+    if(data.isPresent()) {
+      Exam examWidget = data.get();
+      newQuestion.setExam(examWidget);
+      return trueFalseQuestionRepository.save(newQuestion);
+    }
+    return null;
+  }
+
+  @DeleteMapping("/api/truefalse/{qid}")
+  public void deleteTrueFalseQuestion(@PathVariable("qid") int questionId) {
+    trueFalseQuestionRepository.deleteById(questionId);
+  }
 
   @GetMapping("/api/exam")
   public List<Exam> FindAllExams() {
@@ -74,18 +132,6 @@ public class ExamService {
     Optional<TrueFalseQuestion> optional = trueFalseQuestionRepository.findById(questionId);
     if(optional.isPresent()) {
       return optional.get();
-    }
-    return null;
-  }
-
-  @GetMapping("/api/exam/{examId}/question")
-  public List<Question> findAllQuestionsForExam(@PathVariable("examId") int examId) {
-    Optional<Exam> optionalExam = examRepository.findById(examId);
-    if(optionalExam.isPresent()) {
-      Exam exam = optionalExam.get();
-      List<Question> questions = exam.getQuestions();
-      int count = questions.size();
-      return questions;
     }
     return null;
   }
