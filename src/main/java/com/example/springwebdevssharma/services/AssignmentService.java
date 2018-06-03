3bb4794e03025dfddcda79296cf629a018906be1
Babubhai <shubhsharma10@gmail.com>
2018-06-03 00:42:31 -0400

    Added findAssignmentWidget method

--- src/main/java/com/example/springwebdevssharma/services/AssignmentService.java
+++ src/main/java/com/example/springwebdevssharma/services/AssignmentService.java
@@ -2,12 +2,12 @@ package com.example.springwebdevssharma.services;
 
 import com.example.springwebdevssharma.models.Assignment;
 import com.example.springwebdevssharma.models.Topic;
-import com.example.springwebdevssharma.models.Widget;
 import com.example.springwebdevssharma.repositories.AssignmentRepository;
 import com.example.springwebdevssharma.repositories.TopicRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,12 +25,21 @@ public class AssignmentService {
   TopicRepository topicRepository;
 
   @PostMapping("/api/topic/{tid}/assignment")
-  public Widget createAssignment(@PathVariable("tid") int topicId, @RequestBody Assignment newAssignment) {
+  public Assignment createAssignment(@PathVariable("tid") int topicId, @RequestBody Assignment newAssignment) {
     Optional<Topic> data = topicRepository.findById(topicId);
     if(data.isPresent()) {
       Topic topic = data.get();
       newAssignment.setTopic(topic);
       return assignmentRepository.save(newAssignment);
+    }
+    return null;
+  }
+
+  @GetMapping("/api/assignment/{aid}")
+  public Assignment findAssignmentById(@PathVariable("aid") int aid) {
+    Optional<Assignment> data = assignmentRepository.findById(aid);
+    if(data.isPresent()) {
+      return data.get();
     }
     return null;
   }
