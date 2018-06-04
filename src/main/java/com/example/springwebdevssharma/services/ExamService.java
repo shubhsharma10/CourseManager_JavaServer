34f29b3d4864cce07f547a024f75f2b50c34ff5e
Babubhai <shubhsharma10@gmail.com>
2018-06-04 00:15:38 -0400

    Added MutlipleChoice question create/find/update/delete

--- src/main/java/com/example/springwebdevssharma/services/ExamService.java
+++ src/main/java/com/example/springwebdevssharma/services/ExamService.java
@@ -170,6 +170,50 @@ public class ExamService {
     return null;
   }
 
+  ///////////////////////////////////
+  /// Multiple choice Question methods
+  ///////////////////////////////////
+
+  @PostMapping("/api/exam/{eid}/choice")
+  public MultipleChoiceQuestion createMultipleChoiceQuestion(@PathVariable("eid") int examId, @RequestBody MultipleChoiceQuestion newQuestion) {
+    Optional<Exam> data = examRepository.findById(examId);
+    if(data.isPresent()) {
+      Exam examWidget = data.get();
+      newQuestion.setExam(examWidget);
+      return multipleChoiceQuestionRepository.save(newQuestion);
+    }
+    return null;
+  }
+
+  @PutMapping("/api/choice/{qid}")
+  public MultipleChoiceQuestion updateMultipleChoiceQuestion(@PathVariable("qid") int qid, @RequestBody MultipleChoiceQuestion newQuestion) {
+    Optional<MultipleChoiceQuestion> data = multipleChoiceQuestionRepository.findById(qid);
+    if(data.isPresent()) {
+      MultipleChoiceQuestion question = data.get();
+      question.setTitle(newQuestion.getTitle());
+      question.setSubtitle(newQuestion.getSubtitle());
+      question.setPoints(newQuestion.getPoints());
+      question.setCorrectOption(newQuestion.getCorrectOption());
+      question.setOptions(newQuestion.getOptions());
+      return multipleChoiceQuestionRepository.save(question);
+    }
+    return null;
+  }
+
+  @DeleteMapping("/api/choice/{qid}")
+  public void deleteMultipleChoiceQuestion(@PathVariable("qid") int questionId) {
+    multipleChoiceQuestionRepository.deleteById(questionId);
+  }
+
+  @GetMapping("/api/choice/{questionId}")
+  public MultipleChoiceQuestion findMultipleChoiceQuestionById(@PathVariable("questionId") int questionId) {
+    Optional<MultipleChoiceQuestion> optional = multipleChoiceQuestionRepository.findById(questionId);
+    if(optional.isPresent()) {
+      return optional.get();
+    }
+    return null;
+  }
+
 
   /////////////////////////////////////////
 
